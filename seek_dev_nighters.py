3fa4421142c36bf645ceed8fb7ee40d2d545b447
Ranc58 <rancvova@gmail.com>
2017-06-19 21:04:50 +0300

    add some new funcs for print result of script working. add argparse for more user-friendly.

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,35 +1,51 @@
 import requests
 import json
 import datetime as dt
-from itertools import product
+import argparse
+import pytz
+
+FORMAT_TIME = '%H:%M'
+URL_TEMPLATE = 'https://devman.org/api/challenges/solution_attempts/'
+DEFAULT_TIME = '06:00'
+PAGES = 10
+
 
 def load_attempts():
-    pages = 10
-    for page in range(3, pages):
-        api_url = 'https://devman.org/api/challenges/solution_attempts/?page={}'.format(page)
-        response = requests.get(api_url)
-        json_obj = response.content
-        content_list = json.loads(json_obj)
-        yield content_list
+    for page in range(1, PAGES):
+        response = requests.get(URL_TEMPLATE,
+                                params={'page': page})
+        content_massive = response.json()['records']
+        for content in content_massive:
+            if content['timestamp']:
+                yield content
 
 
-def get_users_info(content_list):
-    users_info_list=[{'username': info['username'],
-                      'timestamp': info['timestamp'],
-                      'timezone':  info['timezone']}
-                     for info in content_list['records']]
-    return users_info_list
+def get_midnighters(content, end_time):
+    timezone = pytz.timezone(content['timezone'])
+    delivery_time = dt.datetime.fromtimestamp(content['timestamp'],
+                                              timezone).time()
+    if delivery_time < end_time:
+        yield {'user': content['username'], 'time': delivery_time}
 
 
-def get_midnighters(users_info_list):
-    for user in users_info_list:
-        delivery_time = dt.datetime.fromtimestamp(user['timestamp']).time()
-        if delivery_time < dt.time(6, 0):
-            yield {'user':user['username'],'time':delivery_time}
+def print_midnighters(content, end_time):
+    for user in get_midnighters(content, end_time):
+        print('User "{user}" sent work at {time}'.format(**user))
 
-def print()
+
+def create_parser_for_user_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--time',
+                        help="Up to what time displayed from 00.00 o'clock",
+                        default=DEFAULT_TIME,
+                        type=lambda t:
+                            dt.datetime.strptime(t, FORMAT_TIME).time())
+    namespace = parser.parse_args()
+    return namespace
+
 
 if __name__ == '__main__':
-    for content_list in load_attempts():
-        for user in get_midnighters(get_users_info(content_list)):
-            print('User {user} sent work at {time}'.format(**user))
+    namespace = create_parser_for_user_arguments()
+    end_time = namespace.time
+    for content in load_attempts():
+        print_midnighters(content, end_time)
