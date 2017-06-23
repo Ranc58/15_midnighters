9f7f5526aa505274656702760cc733918d1b0b6e
Ranc58 <rancvova@gmail.com>
2017-06-23 16:38:14 +0300

    edit load_attempts for separate requests between first and next pages

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -8,23 +8,23 @@ URL_TEMPLATE = 'https://devman.org/api/challenges/solution_attempts/'
 DEFAULT_TIME = '06:00'
 
 
-def get_pages_quantity():
-    response = requests.get(URL_TEMPLATE)
-    pages_quantity = response.json()['number_of_pages']
-    return pages_quantity
-
-
-def load_attempts(pages_quantity):
-    for page in range(1, pages_quantity):
-        response = requests.get(URL_TEMPLATE,
-                                params={'page': page})
-        user_info_massive = response.json()['records']
-        for user_info in user_info_massive:
+def load_attempts():
+    get_full_info = requests.get(URL_TEMPLATE).json()
+    pages_quantity = get_full_info['number_of_pages']
+    first_page_info = get_full_info['records']
+    for user_info in first_page_info:
+        if user_info['timestamp']:
+            yield user_info
+    for page in range(2, pages_quantity):
+        params = {'page': page}
+        user_info_massive = requests.get(URL_TEMPLATE,
+                                         params=params).json()
+        for user_info in user_info_massive['records']:
             if user_info['timestamp']:
                 yield user_info
 
 
-def check_midnighters(user_info, end_time):
+def check_midnighter(user_info, end_time):
     timezone = pytz.timezone(user_info['timezone'])
     delivery_time = dt.datetime.fromtimestamp(user_info['timestamp'],
                                               timezone).time()
@@ -34,7 +34,7 @@ def check_midnighters(user_info, end_time):
 
 
 def print_midnighters(user_info, end_time):
-    for user in check_midnighters(user_info, end_time):
+    for user in check_midnighter(user_info, end_time):
         print('User "{user}" sent work at {time}'.format(**user))
 
 
@@ -44,7 +44,7 @@ def create_parser_for_user_arguments():
                         help="Up to what time displayed from 00.00 o'clock",
                         default=DEFAULT_TIME,
                         type=lambda t:
-                            dt.datetime.strptime(t, FORMAT_TIME).time())
+                        dt.datetime.strptime(t, FORMAT_TIME).time())
     namespace = parser.parse_args()
     return namespace
 
@@ -52,6 +52,5 @@ def create_parser_for_user_arguments():
 if __name__ == '__main__':
     namespace = create_parser_for_user_arguments()
     end_time = namespace.time
-    pages_quantity = get_pages_quantity()
-    for user_info in load_attempts(pages_quantity):
+    for user_info in load_attempts():
         print_midnighters(user_info, end_time)
