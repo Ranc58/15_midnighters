6bc3f54430e41067e75acac54eb3b20e6c11ca30
Ranc58 <rancvova@gmail.com>
2017-06-28 21:15:41 +0300

    Delete class for more readable

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -8,34 +8,30 @@ URL_TEMPLATE = 'https://devman.org/api/challenges/solution_attempts/'
 DEFAULT_TIME = '06:00'
 
 
-class Midnighter:
-    def __init__(self, content, end_time):
-        self.content = content
-        self.end_time = end_time
-
-    def get_midnighters(self):
-        timezone = pytz.timezone(self.content['timezone'])
-        delivery_time = dt.datetime.fromtimestamp(self.content['timestamp'],
-                                                  timezone).time()
-        if delivery_time < self.end_time:
-            yield {'user': self.content['username'], 'time': delivery_time}
-
-    def print_midnighters(self):
-        for user in Midnighter.get_midnighters(self):
-            print('User "{user}" sent work at {time}'.format(**user))
-
-
 def load_attempts():
     current_page = 1
     pages_for_parse = 1
     while current_page <= pages_for_parse:
-        response = requests.get(URL_TEMPLATE, params={"page": current_page}).json()
+        response = requests.get(URL_TEMPLATE,
+                                params={"page": current_page}).json()
         pages_for_parse = response["number_of_pages"]
         current_page += 1
         for user_info in response["records"]:
             if user_info['timestamp']:
                 yield user_info
 
+
+def get_midnighters(end_time, content):
+    timezone = pytz.timezone(content['timezone'])
+    delivery_time = dt.datetime.fromtimestamp(content['timestamp'],
+                                              timezone).time()
+    if delivery_time < end_time:
+        yield {'user': content['username'], 'time': delivery_time}
+
+
+def print_midnighters(end_time, content):
+    for user in get_midnighters(end_time, content):
+        print('User "{user}" sent work at {time}'.format(**user))
 
 
 def create_parser_for_user_arguments():
@@ -53,4 +49,4 @@ if __name__ == '__main__':
     namespace = create_parser_for_user_arguments()
     end_time = namespace.time
     for content in load_attempts():
-        Midnighter(content, end_time).print_midnighters()
+        print_midnighters(end_time, content)
